38b82140e530dbdc0ff4aa915869cb1d1c8a1edd
timwang <tim.wang@unicornfintech.com>
2023-07-17 17:47:12 +0800

    update: add delete whole table function

--- utils/get_db.py
+++ utils/get_db.py
@@ -3,6 +3,78 @@ import pandas as pd
 import mysql.connector
 
 
+def delete_table(client, database, table, date_list, where=None):
+    database = database.replace('`','')
+    try:
+        date_list = [ f"'{x}'" for x in date_list]
+        con = mysql.connector.connect(
+            host=client["host"],
+            user=client["user"],
+            password=client["pwd"],
+            database=database,
+            auth_plugin='mysql_native_password'
+        )
+        cursor = con.cursor(buffered=True)
+        del_sql = f'''
+            DELETE 
+            FROM
+            	`{database}`.`{table.replace('`','')}` 
+            WHERE
+            	`date` in ({', '.join(date_list)}) 
+        '''
+        if where != None:
+            del_sql = del_sql + ' ' + where
+        cursor.execute(del_sql)
+        con.commit()
+        res = f'success : delete {database}.{table}'
+    except Exception as e:
+        res = f'failed : delete {database}.{table}'
+        print(e)
+        error = e
+    finally:
+        cursor.close()
+        con.close()
+        print(res)
+        if 'failed' in res:
+            raise Exception(error)
+        return res
+    
+
+
+def delete_whole_table(client, database, table):
+    database = database.replace('`','')
+    try:
+
+        con = mysql.connector.connect(
+            host=client["host"],
+            user=client["user"],
+            password=client["pwd"],
+            database=database,
+            auth_plugin='mysql_native_password'
+        )
+        cursor = con.cursor(buffered=True)
+        del_sql = f'''
+            DELETE 
+            FROM
+            	`{database}`.`{table.replace('`','')}`
+        '''
+
+        cursor.execute(del_sql)
+        con.commit()
+        res = f'success : delete whole {database}.{table}'
+    except Exception as e:
+        res = f'failed : delete whole {database}.{table}'
+        print(e)
+        error = e
+    finally:
+        cursor.close()
+        con.close()
+        print(res)
+        if 'failed' in res:
+            raise Exception(error)
+        return res
+    
+
 
 def write_table(client, database, table, col, df):
     
